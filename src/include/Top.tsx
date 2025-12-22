7efb7545a51dbf17b6193ec20b0110066ec54d1f
hbk <anakyn@naver.com>
2025-12-22 14:14:53 +0900

    커밋

--- src/include/Top.tsx
+++ src/include/Top.tsx
@@ -1,28 +1,47 @@
+import styled from "styled-components";
 import {Container, Row, Col} from "react-bootstrap";
+
+const HeaderWrap = styled.div`
+background-color:white; padding:1rem;
+`;
+
+const Link = styled.a`
+margin-right:1rem;
+text-decoration:none;
+color:black;
+border-bottom :2px solid transparent;
+transition:all 0.5s;
+
+&:hover{
+border-bottom-color:hotpink;
+color:hotpink;
+cursor:pointer;
+}
+`;
 
 const Top = () => {
     return(
         <>
-<div className="header-wrap bg-white p-1">
+<HeaderWrap>
 <Container fluid>
     <Row>
         <Col md={12}>
-<a href="">자금 현황표</a>
-<a href="" className="mx-3">지급어음조회</a>
-<a href="">견적서입력</a>
-<a href="" className="mx-3">구매조회</a>
-<a href="">손익계산서</a>
-<a href="" className="mx-3">판매입력2</a>
-<a href="">재고변동표</a>
-<a href=""className="mx-3">판매입력</a>
-<a href="">재고현황</a>
-<a href=""className="mx-3">일반전표</a>
-<a href="">판매조회</a>
-<a href=""className="mx-3">거래처리스트</a>
+<Link to="">자금 현황표</Link>
+<Link to="">지급어음조회</Link>
+<Link to="">견적서입력</Link>
+<Link to="">구매조회</Link>
+<Link to="">손익계산서</Link>
+<Link to="">판매입력2</Link>
+<Link to="">재고변동표</Link>
+<Link to="">판매입력</Link>
+<Link to="">재고현황</Link>
+<Link to="">일반전표</Link>
+<Link to="">판매조회</Link>
+<Link to="">거래처리스트</Link>
         </Col>
     </Row>
 </Container>
-</div>
+</HeaderWrap>
         </>
     )
 }
